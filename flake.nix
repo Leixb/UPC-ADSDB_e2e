cc88a5a8996cce788a81474f62a7e6ce5a337b43
LeixB <abone9999@gmail.com>
2022-01-05 00:37:23 +0100

    Initialize flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Data Science End-to-End Project";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [ ];
+        };
+      }
+    );
+}
