80fdea2fa9366ffd1904ef5705edde88abd4b01f
LeixB <abone9999@gmail.com>
2022-01-05 01:40:24 +0100

    Add LaTeX document

--- flake.nix
+++ flake.nix
@@ -7,11 +7,76 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        latex-packages = with pkgs; [
+          (texlive.combine {
+            inherit (texlive)
+              scheme-medium
+              framed
+              titlesec
+              cleveref
+              multirow
+              wrapfig
+              tabu
+              threeparttable
+              threeparttablex
+              makecell
+              environ
+              biblatex
+              biber
+              fvextra
+              upquote
+              catchfile
+              xstring
+              csquotes
+              minted
+              dejavu
+              comment
+              footmisc
+              xltabular
+              ltablex
+              ;
+          })
+          which
+          python39Packages.pygments
+        ];
+
+        dev-packages = with pkgs; [
+          texlab
+          zathura
+          wmctrl
+        ];
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = [ ];
+          buildInputs = [ latex-packages dev-packages ];
         };
+        
+        packages = flake-utils.lib.flattenTree {
+          report = with import nixpkgs { inherit system; }; stdenvNoCC.mkDerivation rec {
+            name = "report.pdf";
+            src = ./document;
+
+            buildInputs = [
+              latex-packages
+            ];
+
+            TEXMFHOME="./cache";
+            TEXMFVAR="./cache/var";
+            SOURCE_DATE_EPOCH = toString self.lastModified;
+
+            buildPhase = ''
+              latexmk -lualatex -shell-escape 000-main.tex
+            '';
+
+            installPhase = ''
+              mkdir -p $out
+              mv *.pdf $out/${name}
+            '';
+          };
+        };
+
+        defaultPackage = self.packages.${system}.report;
       }
     );
 }
