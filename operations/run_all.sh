aa6de4bd2508fb672a5292827c45eb52111fa020
LeixB <abone9999@gmail.com>
2022-01-23 22:26:43 +0100

    Add run_all.sh

---
+++ operations/run_all.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+TEMPORAL_FOLDER=${1:-landing/temporal}
+
+mkdir -p "$TEMPORAL_FOLDER"
+
+./download.sh "$TEMPORAL_FOLDER"
+
+echo START LANDING
+python landing.py "$TEMPORAL_FOLDER"
+
+echo START FORMATTED
+python formatted.py
+
+echo START FORMATTED
+python trusted.py
+
+echo START EXPLOITATION
+python exploitation.py
+
+echo START MODELLING
+python modelling.py
+
+echo DONE
